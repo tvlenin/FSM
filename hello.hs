2637430236e55d1fcb9c2954a1ae59ebbe88f8ee
tvlenin <ttvleninn@gmail.com>
2016-05-26 16:33:21 -0600

    he

--- hello.hs
+++ hello.hs
@@ -1,4 +1,5 @@
 import Data.List
+import Data.List.Split
 import Data.Char
 main:: IO()
 
@@ -7,9 +8,9 @@ main = do
 	body ["/","/"]
 
 body xe = do 
-	putStrLn "Las carpetas actuales son"
+	--putStrLn "Las carpetas actuales son"
 	putStrLn $ show xe
-	putStrLn "Digite in para ingresar o find para buscar"
+	--putStrLn "Digite in para ingresar o find para buscar"
 	op <- getLine
 	
 	if op == "in" then do
@@ -19,50 +20,64 @@ body xe = do
 		
 	else if op == "ls" then do
 		listFiles 0 xe
-		--line <- getLine
-		--findFiles xe line
+		
+	else if op == "cd" then do
+		line <- getLine
+		if line == ".." then do
+			moveDirectory ("..") xe
+		else do
+			moveDirectory (line) xe
 		
 	else if op == "find" then do
 		putStrLn "Digite el nombre de la carpeta a buscar"
-		--line <- getLine
-		--findFiles xe line
-	
+			
 	else do
 		putStrLn $ "digite el comando correcto"
 		body xe		
 ----------------------------------------------------------------
 --addFiles create a new directory in the actual 	
 addFiles xe add  = do
-	let tmp = xe ++ [head xe ++ add]
-	putStrLn $ show tmp
-	body tmp 
+	if (head xe == "/") then do
+		body (xe ++ [head xe ++ add])
+	else do
+		body (xe ++ [head xe++"/" ++ add])		 
 ----------------------------------------------------------------	  
 --called with ls command show all the  directories in the actual
 listFiles cont xe = do
 	if (cont < length xe) then do
-
 		if (head xe `isInfixOf` (xe !! cont)) && (head xe /= (xe !! cont))  then do
 			putStrLn $ xe !! cont
-			let tmp = addOne cont
-			listFiles tmp xe
-	
-	
+			listFiles (cont + 1) xe
 		else do 
-			let tmp = addOne cont
-			listFiles tmp xe
+			listFiles (cont +1 ) xe
 	else do
 		body xe
 ---------------------------------------------------------------
 --move to a directory in the actual
 moveDirectory dir xe = do
-	if dir `elem` xe then do
-		let tmp = dir : tail xe
-		body tmp
+	if dir == ".." then do 
+		if (head xe == "/") then 
+			body xe
+		else do
+			delLast xe	
+	else if ((head xe == "/") &&(((head xe)++ dir)) `elem` xe) then do
+		body (((head xe)++ dir):tail xe)
+	else if ((head xe)++"/"++ dir) `elem` xe then do
+		body (((head xe)++"/"++ dir):tail xe)
 	else do
 		putStrLn "No existe"
+		body xe
 	
 ---------------------------------------------------------------
-addOne x = x + 1
+--move to previous directory
+delLast xe = do
+	if ([last(head xe)] /= "/") then do 
+		delLast ((init (head xe)): (tail xe)) 
+	else if ([last(head xe)] == head xe) then do 
+		body xe 
+	else do
+		body ((init (head xe)): (tail  xe))
+		
 	
 	
 
